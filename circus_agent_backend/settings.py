27c4652b3c2b0c7b10b9fb91e30dd52967ab38de
philippeducasse <ducassephi@hotmail.fr>
2025-11-09 21:31:24 +0100

    wip

--- circus_agent_backend/settings.py
+++ circus_agent_backend/settings.py
@@ -121,22 +121,22 @@ WSGI_APPLICATION = "circus_agent_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "cab",
-        "USER": "philippe",
-        "PASSWORD": "0306",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "cab",
+#         "USER": "philippe",
+#         "PASSWORD": "0306",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
