e7f74378ad6761b1ec7f8868ba772f6fccf9868b
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2025-11-27 16:27:23 +0100

    added auth endpoints

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework import viewsets, status, permissions
-from rest_framework.request import Request
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import QuerySet
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+
 from profiles.models import Profile
 from profiles.serializers import ProfileSerializer, RegisterSerializer
 
@@ -54,3 +56,24 @@ class ProfileViewSet(viewsets.ModelViewSet):
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.erors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["post"], permission_classes=[permissions.AllowAny])
+    def login(self, request: Request) -> Response:
+        email = request.data.get("email")
+        password = request.data.get("password")
+        print("password: ", password)
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+
+        return Response(
+            {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    @action(detail=False, methods=["post"])
+    def logout(self, request):
+        logout(request)
+        return Response({"message": "Logged out successfully"})
