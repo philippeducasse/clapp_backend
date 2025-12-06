ee74b7e0fd6f937729b61ebdd16cc263c8186065
philippeducasse <ducassephi@hotmail.fr>
2025-12-06 13:32:28 +0100

    fixed performance serialiser

--- performances/serializers.py
+++ performances/serializers.py
@@ -5,6 +5,11 @@ from typing import Type
 
 class PerformanceSerializer(serializers.ModelSerializer):
     creation_date = serializers.DateField(allow_null=True, required=False)
+    genres = serializers.ListField(
+        child=serializers.CharField(),
+        required=False,
+        allow_empty=True
+    )
 
     class Meta:
         model: Type[Performance] = Performance
