85a25b16936e5658361d01b785f0611d12043d46
philippeducasse <ducassephi@hotmail.fr>
2026-02-27 11:57:34 +0100

    chore(urls): added forgot pw endpoints

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,12 +3,20 @@ from typing import List, Union
 from django.urls import URLPattern, URLResolver, include, path
 from rest_framework.routers import DefaultRouter
 
-from profiles.views import ProfileViewSet, ReminderViewSet, confirm_email
+from profiles.views import (
+    ProfileViewSet,
+    ReminderViewSet,
+    confirm_email,
+    forgot_password,
+    reset_password,
+)
 
 router: DefaultRouter = DefaultRouter()
 router.register(r"", ProfileViewSet, basename="profile")
 router.register(r"me/reminders", ReminderViewSet, basename="reminder")
 urlpatterns: List[Union[URLPattern, URLResolver]] = [
     path("confirm-email/", confirm_email, name="confirm-email"),
+    path("reset-password/", reset_password, name="confirm-email"),
+    path("forgot-password/", forgot_password, name="forgot-password"),
     path("", include(router.urls)),
 ]
